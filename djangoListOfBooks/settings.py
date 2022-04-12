a77974521f60834885008ce3e995a8c32b216624
aleksandra_1508 <ola.kiel@onet.pl>
2022-04-12 17:23:51 +0200

    add5

--- djangoListOfBooks/settings.py
+++ djangoListOfBooks/settings.py
@@ -122,9 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT =[
-    BASE_DIR / 'staticfiles'
-]
+STATIC_ROOT = [Path.joinpath(BASE_DIR, 'staticfiles')]
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
